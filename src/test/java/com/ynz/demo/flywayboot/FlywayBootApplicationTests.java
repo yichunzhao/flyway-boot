9f6574a3dde24697c6337f8a2cf41cfc95fff664
yichunzhao <zhaoyichun@hotmail.com>
2021-06-01 11:19:21 +0200

    Initial commit

---
+++ src/test/java/com/ynz/demo/flywayboot/FlywayBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.flywayboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
